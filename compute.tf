2aeae9ac05a06e81ceebe532d2120af54dc2818f
ckelner <chris.kelner@gmail.com>
2017-12-01 20:42:58 -0500

    Add random shuffle for subnet

--- compute.tf
+++ compute.tf
@@ -3,8 +3,16 @@
 # https://www.terraform.io/docs/providers/aws/r/key_pair.html
 #
 resource "aws_key_pair" "aws_ssh_key" {
-  key_name   = "${var.common_name}-${terraform.env}"
+  key_name   = "${var.common_name}-${terraform.workspace}"
   public_key = "${var.aws_public_key_material}"
+}
+#
+# Random Shuffle
+# https://www.terraform.io/docs/providers/random/r/shuffle.html
+#
+resource "random_shuffle" "subnet" {
+  input = ["${aws_subnet.public.*.id}"]
+  result_count = 1
 }
 #
 # AWS EC2 Instance
@@ -17,6 +25,7 @@ resource "aws_instance" "web" {
     "${aws_security_group.web_sg.id}",
     "${module.http_security_group.id}"
   ]
+  subnet_id = "${random_shuffle.subnet.result[0]}"
   user_data = <<-EOF
               #!/bin/bash
               sudo yum install nginx -y
