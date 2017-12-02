681c6f88b16804ebbe3c774bf5ba3cf12982c577
ckelner <chris.kelner@gmail.com>
2017-12-01 20:43:28 -0500

    Fix subnets & add ca-central-1

--- variables.tf
+++ variables.tf
@@ -15,27 +15,20 @@ variable "aws_vpc_cidr_block" {
 }
 variable "aws_vpc_cidr_public_subnets" {
   description = "The public subnet CIDR blocks"
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 variable "aws_amis" {
   description = "AWS AMIs for Amazon Linux 2017.03.0 (HVM) with SSD Volume."
   default = {
-    "us-east-2" = "ami-4191b524"
-    "us-west-2" = "ami-4836a428"
+    "us-east-1" = "ami-55ef662f"
+    "us-east-2" = "ami-15e9c770"
+    "us-west-2" = "ami-bf4193c7"
+    "ca-central-1" = "ami-d29e25b6"
   }
 }
 variable "aws_instance_type" {
   default = "t2.nano"
   description = "The AWS instance type to launch."
-}
-variable "aws_asg_min" {
-  description = "Minimum number of servers in AWS ASG."
-}
-variable "aws_asg_max" {
-  description = "Maximum numbers of servers in AWS ASG."
-}
-variable "aws_asg_desired" {
-  description = "Desired numbers of servers in ASG"
 }
 variable "aws_public_key_material" {
   description = "The public SSH key material to load onto the instances."
